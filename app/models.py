b230cbf775104b2f5017d0e93a4ed14c64dd80ca
natashanarang <nnarang777@gmail.com>
2015-09-25 16:20:47 -0400

    Final project code

--- app/models.py
+++ app/models.py
@@ -2,40 +2,13 @@ from . import db
 from datetime import datetime
 
 class Coordinate(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.Float, unique=True)
-    longitutde = db.Column(db.Float, unique=True)
-    notes = db.Column(db.String(500), unique=True)
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    notes = db.Column(db.String(500))
 
     def __init__(self, latitude, longitude, notes):
         self.latitude = latitude
-        self.longitue = longitude
+        self.longitude = longitude
         self.notes = notes
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def is_cool(self):
-        return (self.username == 'aharelick')
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    body = db.Column(db.Text)
-    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post %r>' % self.title
